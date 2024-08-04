dfc86833c847dc65db7ae74ea2a552733eb47bb2
Timfedora <168787880+Timfedora@users.noreply.github.com>
2024-08-03 22:10:30 -0400

    Update Software.sh

--- Software.sh
+++ Software.sh
@@ -1,65 +1,47 @@
-#!/bin/sh
+# Software-Installer
 
-echo "Please enter your laptop's password"
-sudo echo
+Software-Installer is a Bash script designed to automate the installation of various software packages on a Linux system. It allows you to select and install multiple packages in one go, simplifying the setup process.
 
-echo "Software list"
-echo "Text editors"
-echo "(1) atom"
-echo "(2) pluma"
-echo "(3) gedit"
-echo "(4) emacs"
-echo "(5) vim"
-echo "(6) kwrite"
-echo "(7) vscode"
-echo "Audio & Video"
-echo "(8) mpv"
-echo "(9) audacity"
-echo "Electronics"
-echo "(10) qucs"
-echo "(11) kicad"
-echo "Terminals"
-echo "(12) tilix"
-echo "(13) konsole"
-echo "Other"
-echo "(14) gparted"
+## Purpose
 
-echo "Please enter the numbers of the Softwares you would like with spaces"
-read -r selections
+Installing software on a Linux system can sometimes be a tedious process, especially when multiple packages are needed. This script automates the installation of commonly used software, making it easier to get your system up and running with the tools you need.
 
-install_software() {
-    case "$1" in
-        1) 
-            echo "Downloading Atom..."
-            curl -L -o atom.x86_64.rpm https://github.com/atom/atom/releases/download/v1.60.0/atom.x86_64.rpm
-            sudo dnf install atom.x86_64.rpm -y
-            rm atom.x86_64.rpm ;;
-        2) sudo dnf install pluma -y ;;
-        3) sudo dnf install gedit -y ;;
-        4) sudo dnf install emacs -y ;;
-        5) sudo dnf install vim -y ;;
-        6) sudo dnf install kwrite -y ;;
-        7) 
-            echo "Downloading Visual Studio Code..."
-            curl -L -o vscode.rpm 'https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64'
-            sudo dnf install vscode.rpm -y
-            rm vscode.rpm ;;
-        8) sudo dnf install mpv -y ;;
-        9) sudo dnf install audacity -y ;;
-        10) sudo dnf install qucs -y ;;
-        11) 
-            echo "Installing KiCad..."
-            sudo dnf install -y dnf-plugins-core
-            sudo dnf copr enable -y @kicad/kicad-testing
-            sudo dnf install -y kicad kicad-packages3d kicad-doc ;;
-        12) sudo dnf install tilix -y ;;
-        13) sudo dnf install konsole -y ;;
-        14) sudo dnf install gparted -y ;;
-        *) echo "Invalid selection" ;;
-    esac
-}
+## Supported Software
 
-for selection in $selections; do
-    install_software "$selection"
-done
+- **Text Editors:**
+  - Atom
+  - Pluma
+  - Gedit
+  - Emacs
+  - Vim
+  - Kwrite
+  - Visual Studio Code
 
+- **Audio & Video:**
+  - MPV
+  - Audacity
+
+- **Electronics:**
+  - Qucs
+  - KiCad
+
+- **Terminals:**
+  - Tilix
+  - Konsole
+
+- **Other:**
+  - GParted
+
+## Prerequisites
+
+- A Unix-like operating system (Linux).
+- `dnf` package manager (used for Fedora, CentOS, RHEL).
+- `curl` (for downloading packages).
+- `sudo` privileges for installing software.
+
+## Installation
+
+1. **Clone or Download the Script:**
+
+   ```bash
+   git clone https://github.com/your-username/Software-Installer.git
